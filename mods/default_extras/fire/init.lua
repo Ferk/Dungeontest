69350dbb8032193bb6a06e9c8b49a098bc437249
Fernando Carmona Varo <ferkiwi@gmail.com>
2015-10-17 20:17:42 +0200

    Made fire quicker to spread, burn blocks and extinguish, some experimentation on optimizations for the way the sound is calculated

--- mods/default_extras/fire/init.lua
+++ mods/default_extras/fire/init.lua
@@ -16,11 +16,13 @@ minetest.register_node("fire:basic_flame", {
 			aspect_w = 16, aspect_h = 16, length = 1},
 	}},
 	inventory_image = "fire_basic_flame.png",
+	paramtype = "light",
 	light_source = 14,
 	groups = {igniter = 2, dig_immediate = 3},
 	drop = '',
 	walkable = false,
 	buildable_to = true,
+	sunlight_propagates = true,
 	damage_per_second = 4,
 
 	on_construct = function(pos)
@@ -31,21 +33,13 @@ minetest.register_node("fire:basic_flame", {
 		minetest.after(0, fire.on_flame_remove_at, pos)
 	end,
 
-	-- unaffected by explosions
-	on_blast = function() end,
+	on_blast = function() end, -- unaffected by explosions
 })
-
-
--- Fire sounds table
--- key: position hash of low corner of area
--- value: {handle=sound handle, name=sound name}
-fire.sounds = {}
 
 
 -- Get sound area of position
 
--- size of sound areas
-fire.D = 6
+fire.D = 6 -- size of sound areas
 
 function fire.get_area_p0p1(pos)
 	local p0 = {
@@ -62,43 +56,60 @@ function fire.get_area_p0p1(pos)
 end
 
 
--- Update fire sounds in sound area of position
+-- Fire burning areas table (area size is fire.D)
+-- key: position hash of low corner of area
+-- value: {sound=sound handle, name=sound name, density=flame node counter}
+fire.burn_areas = {}
 
-function fire.update_sounds_around(pos)
+
+-- Update fire density and sounds in burn area of position
+-- diff: integer to add to the density of the area
+
+function fire.update_burn_area(pos, diff)
 	local p0, p1 = fire.get_area_p0p1(pos)
-	local cp = {x = (p0.x + p1.x) / 2, y = (p0.y + p1.y) / 2, z = (p0.z + p1.z) / 2}
-	local flames_p = minetest.find_nodes_in_area(p0, p1, {"fire:basic_flame"})
-	--print("number of flames at "..minetest.pos_to_string(p0).."/"
-	--		..minetest.pos_to_string(p1)..": "..#flames_p)
-	local should_have_sound = (#flames_p > 0)
-	local wanted_sound = nil
-	if #flames_p >= 9 then
-		wanted_sound = {name = "fire_large", gain = 1.5}
-	elseif #flames_p > 0 then
-		wanted_sound = {name = "fire_small", gain = 1.5}
-	end
 	local p0_hash = minetest.hash_node_position(p0)
-	local sound = fire.sounds[p0_hash]
-	if not sound then
-		if should_have_sound then
-			fire.sounds[p0_hash] = {
-				handle = minetest.sound_play(wanted_sound,
-					{pos = cp, max_hear_distance = 16, loop = true}),
-				name = wanted_sound.name,
-			}
-		end
+
+	local burnarea = fire.burn_areas[p0_hash]
+	if not burnarea then
+		-- If we don't have a previous saved state, calculate it
+		burnarea = { density = 0 }
+		fire.burn_areas[p0_hash] = burnarea
+	end
+	local density = burnarea.density + diff
+
+	-- Negative density can happen if the flames were already there on world load
+	-- In this case, check for the flames in the area properly
+	if density < 0 then
+		local flames_p = minetest.find_nodes_in_area(p0, p1, {"fire:basic_flame"})
+		density = #flames_p
+	end
+
+	local sound = false
+	if density >= 9 then
+		sound = {name = "fire_large", gain = 1.5}
+	elseif density > 0 then
+		sound = {name = "fire_small", gain = 1.5}
 	else
-		if not wanted_sound then
-			minetest.sound_stop(sound.handle)
-			fire.sounds[p0_hash] = nil
-		elseif sound.name ~= wanted_sound.name then
-			minetest.sound_stop(sound.handle)
-			fire.sounds[p0_hash] = {
-				handle = minetest.sound_play(wanted_sound,
-					{pos = cp, max_hear_distance = 16, loop = true}),
-				name = wanted_sound.name,
-			}
+		-- remove sound and saved state if the density is 0
+		if burnarea.sound then
+			minetest.sound_stop(burnarea.sound)
 		end
+		fire.burn_areas[p0_hash] = nil
+		return
+	end
+	burnarea.density = density
+
+	if sound and sound.name ~= burnarea.name then
+		if burnarea.sound then
+			minetest.sound_stop(burnarea.sound)
+		end
+		local cp = {x = (p0.x + p1.x) / 2, y = (p0.y + p1.y) / 2, z = (p0.z + p1.z) / 2}
+		burnarea.name = sound.name
+		burnarea.sound = minetest.sound_play(sound, {
+			pos = cp,
+			max_hear_distance = fire.D,
+			loop = true
+		})
 	end
 end
 
@@ -106,12 +117,12 @@ end
 -- Update fire sounds on flame node construct or destruct
 
 function fire.on_flame_add_at(pos)
-	fire.update_sounds_around(pos)
+	fire.update_burn_area(pos, 1)
 end
 
 
 function fire.on_flame_remove_at(pos)
-	fire.update_sounds_around(pos)
+	fire.update_burn_area(pos, -1)
 end
 
 
@@ -125,33 +136,98 @@ end
 -- Detect nearby extinguishing nodes
 
 function fire.flame_should_extinguish(pos)
-	if minetest.setting_getbool("disable_fire") then return true end
-	--return minetest.find_node_near(pos, 1, {"group:puts_out_fire"})
-	local p0 = {x = pos.x - 1, y = pos.y, z = pos.z - 1}
-	local p1 = {x = pos.x + 1, y = pos.y + 1, z = pos.z + 1}
-	local ps = minetest.find_nodes_in_area(p0, p1, {"group:puts_out_fire"})
-	return (#ps ~= 0)
+	return minetest.find_node_near(pos, 1, {"group:puts_out_fire"})
 end
 
 
--- Ignite neighboring nodes
+-- Enable ABMs according to 'disable fire' setting
 
-minetest.register_abm({
-	nodenames = {"group:flammable"},
-	neighbors = {"group:igniter"},
-	interval = 7,
-	chance = 32,
-	action = function(p0, node, _, _)
-		-- If there is water or stuff like that around flame, don't ignite
-		if fire.flame_should_extinguish(p0) then
-			return
-		end
-		local p = fire.find_pos_for_flame_around(p0)
-		if p then
-			minetest.set_node(p, {name = "fire:basic_flame"})
-		end
-	end,
-})
+if minetest.setting_getbool("disable_fire") then
+
+	-- Extinguish flames quickly with dedicated ABM
+
+	minetest.register_abm({
+		nodenames = {"fire:basic_flame"},
+		interval = 3,
+		chance = 2,
+		action = function(p0, node, _, _)
+			minetest.remove_node(p0)
+		end,
+	})
+
+else
+
+	-- Extinguish flames quickly with water, snow, ice
+
+	minetest.register_abm({
+		nodenames = {"fire:basic_flame"},
+		neighbors = {"group:puts_out_fire"},
+		interval = 3,
+		chance = 2,
+		action = function(p0, node, _, _)
+			minetest.remove_node(p0)
+			minetest.sound_play("fire_extinguish_flame",
+				{pos = p0, max_hear_distance = 16, gain = 0.25})
+		end,
+	})
+
+	-- Ignite neighboring nodes
+
+	minetest.register_abm({
+		nodenames = {"group:flammable"},
+		neighbors = {"group:igniter"},
+		interval = 7,
+		chance = 16,
+		action = function(p0, node, _, _)
+			-- If there is water or stuff like that around node, don't ignite
+			if fire.flame_should_extinguish(p0) then
+				return
+			end
+			local p = fire.find_pos_for_flame_around(p0)
+			if p then
+				minetest.set_node(p, {name = "fire:basic_flame"})
+			end
+		end,
+	})
+
+	-- Remove flames and flammable nodes (customizable interval)
+
+	local fire_update_interval = tonumber(minetest.setting_get("fire_update_interval"))
+	if fire_update_interval == 0 then
+		fire_update_interval = 2
+	end
+
+	minetest.register_abm({
+		nodenames = {"fire:basic_flame"},
+		interval = fire_update_interval,
+		chance = 4,
+		action = function(p0, node, _, _)
+			-- If there is water or stuff like that around flame, remove flame
+			if fire.flame_should_extinguish(p0) then
+				minetest.remove_node(p0)
+				return
+			end
+			-- Make the following things rarer
+			if math.random(1, 3) == 1 then
+				return
+			end
+
+			-- If there are no flammable nodes around flame, remove flame
+			local p = minetest.find_node_near(p0, 1, {"group:flammable"})
+			if not p then
+				minetest.remove_node(p0)
+			elseif math.random(1, 2) == 1 then
+				-- remove the flammable node found around flame
+				minetest.remove_node(p)
+				nodeupdate(p)
+			elseif math.random(1, 2) == 1 then
+				-- We might still randomly remove the flame
+				minetest.remove_node(p0)
+			end
+		end,
+	})
+
+end
 
 
 -- Rarely ignite things from far
@@ -184,44 +260,3 @@ minetest.register_abm({
 	end,
 })
 --]]
-
-
--- Remove flammable nodes and flame
-
-minetest.register_abm({
-	nodenames = {"fire:basic_flame"},
-	interval = 5,
-	chance = 16,
-	action = function(p0, node, _, _)
-		-- If there is water or stuff like that around flame, remove flame
-		if fire.flame_should_extinguish(p0) then
-			minetest.remove_node(p0)
-			return
-		end
-		-- Make the following things rarer
-		if math.random(1, 3) == 1 then
-			return
-		end
-		-- If there are no flammable nodes around flame, remove flame
-		if not minetest.find_node_near(p0, 1, {"group:flammable"}) then
-			minetest.remove_node(p0)
-			return
-		end
-		if math.random(1, 4) == 1 then
-			-- remove a flammable node around flame
-			local p = minetest.find_node_near(p0, 1, {"group:flammable"})
-			if p then
-				-- If there is water or stuff like that around flame, don't remove
-				if fire.flame_should_extinguish(p0) then
-					return
-				end
-				minetest.remove_node(p)
-				nodeupdate(p)
-			end
-		else
-			-- remove flame
-			minetest.remove_node(p0)
-		end
-	end,
-})
-
